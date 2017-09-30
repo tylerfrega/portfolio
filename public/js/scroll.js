eb18a7a020611d23c16996f5c082a494c0f28b53
Tyler J. Frega <tyfrega@gmail.com>
2017-09-29 23:28:54 -0400

    finished modals

--- public/js/scroll.js
+++ public/js/scroll.js
@@ -1,3 +1,15 @@
+$(document).ready(function(){
+    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+    $('.modal').modal();
+    
+            //now you can open modal from code
+            $('.trigger-modal').modal();
+    
+  });
+
+
+
+
 $("#aboutMeBtn").click(function() {
     $('html,body').animate({
         scrollTop: $("#aboutMe").offset().top},
